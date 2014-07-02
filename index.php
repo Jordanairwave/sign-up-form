83a3cb59ac222499f384f2e900926d2aebd5c93d
Jordan Haines <jordan@jordanairwave.co.uk>
2014-07-02 16:49:48 +0100

    base files added
    Signed-off-by: Jordan Haines <jordan@jordanairwave.co.uk>

---
+++ index.php
@@ -0,0 +1,60 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<title>Title Here</title>
+	<meta charset="utf-8">
+	<meta name="description" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<link href="css/style.css?v=1.0" rel="stylesheet" type="text/css" />
+	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
+	<!-- Google -->
+	<link rel=”author” href=”https://plus.google.com/[YOUR PERSONAL G+ PROFILE HERE]“/>
+	<!-- Facebook -->
+	<meta property=”og:title” content=”[TITLE OF PAGE HERE]”/>
+	<meta property=”og:type” content=”website”/>
+	<meta property=”og:image” content=”[LINK TO THUMBNAIL MUST BE SQUARE 154PX X 154PX]”/>
+	<meta property=”og:url” content=”[URL OF CURRENT PAGE]”/>
+	<meta property=”og:description” content=”[DESCRIPTION OF PAGE]”/>
+	<meta property=”fb:admins” content=”USER_ID”/>
+	<!-- Twitter -->
+	<meta name=”twitter:card” content=”summary”>
+	<meta name=”twitter:url” content=”[URL OF CURRENT PAGE]”>
+	<meta name=”twitter:title” content=”[TITLE OF PAGE HERE]”>
+	<meta name=”twitter:description” content=”[DESCRIPTION - 200 CHARACTERS MAX LENGTH]”>
+	<meta name=”twitter:image” content=”[IMAGE TO BE SHARED, MUST BE SQUARE 154PX X 154PX]”>
+	<script>
+	  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	  s.parentNode.insertBefore(g,s)}(document,'script'));
+	</script>
+</head>
+<body>
+	<div id="page-wrap">
+		<header id="header">
+			<nav id="main-nav">
+			
+			</nav>
+		</header>
+		<section id="main-content">
+			
+		</section>
+		<aside id="sidebar">
+			
+			
+		</aside>
+		<footer id="footer">
+			
+		</footer>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/modernizr.custom.44232.js"></script>
+	<script type="text/javascript" src="js/scripts.js?v=1.0"></script>
+</body>
+
+</html>
